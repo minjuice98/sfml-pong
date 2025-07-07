45c78cfade2edc52477973ad721fd5222decdfb8
김민주 <1105kmj@naver.com>
2025-07-07 09:43:59 +0900

    2play.

--- Ball.cpp
+++ Ball.cpp
@@ -55,7 +55,7 @@ void Ball::Release()
 void Ball::Reset()
 {
     sf::FloatRect bounds = FRAMEWORK.GetWindowBounds();
-    SetPosition({ bounds.width * 0.5f, bounds.height * 0.5f });
+    //SetPosition({ bounds.width * 0.5f, bounds.height * 0.5f });
 
     float radius = shape.getRadius();
     minX = bounds.left + radius;
@@ -71,7 +71,6 @@ void Ball::Update(float dt)
 {
     sf::Vector2f pos = GetPosition() + direction * speed * dt;
 
-    // 득점 판정
     if (pos.x < minX)
     {
         if (SCENE_MGR.GetCurrentSceneId() == SceneIds::Game)
@@ -101,7 +100,6 @@ void Ball::Update(float dt)
         return;
     }
 
-    // 상하 벽 충돌
     if (pos.y < minY)
     {
         pos.y = minY;
@@ -113,26 +111,24 @@ void Ball::Update(float dt)
         direction.y *= -1.f;
     }
 
-    // Bat1 충돌
     if (bat1 != nullptr)
     {
         const sf::FloatRect& batBounds = bat1->GetGlobalBounds();
         if (shape.getGlobalBounds().intersects(batBounds))
         {
             pos.x = batBounds.left + batBounds.width + shape.getRadius();
-            direction.x = std::abs(direction.x); // 오른쪽으로 반사
-            direction.y *= -1.f; // 각도 약간 바꾸기
+            direction.x = std::abs(direction.x); 
+            direction.y *= -1.f; 
         }
     }
 
-    // Bat2 충돌
     if (bat2 != nullptr)
     {
         const sf::FloatRect& batBounds = bat2->GetGlobalBounds();
         if (shape.getGlobalBounds().intersects(batBounds))
         {
             pos.x = batBounds.left - shape.getRadius();
-            direction.x = -std::abs(direction.x); // 왼쪽으로 반사
+            direction.x = -std::abs(direction.x); 
             direction.y *= -1.f;
         }
     }
